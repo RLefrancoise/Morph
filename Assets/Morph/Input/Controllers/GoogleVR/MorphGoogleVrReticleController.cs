7aab1fff638702e4cc73529b43d084a79e8d7da9
Renaud Lefrançoise <renaud.lefrancoise@gmail.com>
2018-10-16 22:36:06 +0200

    Reticle controller

---
+++ Assets/Morph/Input/Controllers/GoogleVR/MorphGoogleVrReticleController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Morph.Input.Controllers.Common;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Morph.Input.Controllers.GoogleVR
+{
+    public class MorphGoogleVrReticleController : MorphControllerWithEventTrigger
+    {
+        public override MorphControllerFeatures SupportedFeatures => MorphControllerFeatures.PositionTracking | MorphControllerFeatures.RotationTracking;
+        protected override Ray GrabbedRay => new Ray(transform.position, transform.forward);
+        protected override Predicate<BaseEventData> SelectValidation => eventData => true;
+        protected override Predicate<BaseEventData> DeselectValidation => eventData => true;
+    }
+}
