edeb4cbed7daa5d6e567a0d92f65a9ca2cce7068
Renaud Lefrançoise <renaud.lefrancoise@gmail.com>
2018-10-25 18:32:16 +0200

    Add scene to application data

--- Assets/Morph/Editor/MorphApplicationWindow.cs
+++ Assets/Morph/Editor/MorphApplicationWindow.cs
@@ -13,7 +13,31 @@ namespace Morph.Editor
         public struct ApplicationData
         {
             public string Name;
+            public string Scene;
             public List<string> Dependencies;
+
+            public override bool Equals(object obj)
+            {
+                return obj != null && Equals( (ApplicationData) obj);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    var hashCode = (Name != null ? Name.GetHashCode() : 0);
+                    hashCode = (hashCode * 397) ^ (Scene != null ? Scene.GetHashCode() : 0);
+                    hashCode = (hashCode * 397) ^ (Dependencies != null ? Dependencies.GetHashCode() : 0);
+                    return hashCode;
+                }
+            }
+
+            public bool Equals(ApplicationData other)
+            {
+                return string.Equals(Name, other.Name) && Equals(Dependencies, other.Dependencies);
+            }
+
+            
         }
 
         private List<ApplicationData> _applications;
