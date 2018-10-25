f897e34e01eb14c1daf33a65ffb92fb3e0b5ab14
Renaud Lefrançoise <renaud.lefrancoise@gmail.com>
2018-10-26 00:19:51 +0200

    Continue Morph application window

---
+++ Assets/Morph/Editor/FileUtils.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+namespace Morph.Editor
+{
+    public static class FileUtils
+    {
+        /// <summary>
+        /// Test if a folder exists, and create it if not
+        /// </summary>
+        /// <param name="folderPath">Path of the folder</param>
+        /// <returns>Info of the folder, null if no folder is created or found</returns>
+        public static DirectoryInfo CreateFolderIfNeeded(string folderPath)
+        {
+            bool hasDirectory = Directory.Exists(folderPath);
+            if (!hasDirectory)
+            {
+                Debug.LogWarningFormat("Morph couldn't find directory {0}. Creating one.", folderPath);
+                DirectoryInfo info = Directory.CreateDirectory(folderPath);
+                if (!info.Exists)
+                {
+                    Debug.LogErrorFormat("Morph failed to create directory at path {0}", folderPath);
+                    return null;
+                }
+
+                return info;
+            }
+
+            return new DirectoryInfo(folderPath);
+        }
+    }
+}
--- Assets/Morph/Editor/MorphApplicationWindow.cs
+++ Assets/Morph/Editor/MorphApplicationWindow.cs
@@ -1,89 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Morph.Core;
 using UnityEditor;
+using UnityEditor.Build.Content;
 using UnityEngine;
 
 namespace Morph.Editor
 {
+    /// <inheritdoc />
+    /// <summary>
+    /// Morph Application window. You can configure the current application in this window.
+    /// </summary>
     public class MorphApplicationWindow : EditorWindow
     {
-        [Serializable]
-        public struct ApplicationData
-        {
-            public string Name;
-            public string Scene;
-            public List<string> Dependencies;
-
-            public override bool Equals(object obj)
-            {
-                return obj != null && Equals( (ApplicationData) obj);
-            }
-
-            public override int GetHashCode()
-            {
-                unchecked
-                {
-                    var hashCode = (Name != null ? Name.GetHashCode() : 0);
-                    hashCode = (hashCode * 397) ^ (Scene != null ? Scene.GetHashCode() : 0);
-                    hashCode = (hashCode * 397) ^ (Dependencies != null ? Dependencies.GetHashCode() : 0);
-                    return hashCode;
-                }
-            }
-
-            public bool Equals(ApplicationData other)
-            {
-                return string.Equals(Name, other.Name) && Equals(Dependencies, other.Dependencies);
-            }
-
-            
-        }
+        #region Fields
 
         private List<ApplicationData> _applications;
-
         private int _selectedApplication;
+        private List<string> _missingDependencies;
 
-        [MenuItem("Morph/Application")]
-        public static void ShowWindow()
-        {
-            EditorWindow.GetWindow<MorphApplicationWindow>();
-        }
+        #endregion
 
-        private DirectoryInfo CreateFolderIfNeeded(string folderPath)
-        {
-            bool hasDirectory = Directory.Exists(folderPath);
-            if (!hasDirectory)
-            {
-                Debug.LogWarningFormat("Morph couldn't find directory {0}. Creating one.", folderPath);
-                DirectoryInfo info = Directory.CreateDirectory(folderPath);
-                if (!info.Exists)
-                {
-                    Debug.LogErrorFormat("Morph failed to create directory at path {0}", folderPath);
-                    return null;
-                }
+        #region Properties
 
-                return info;
-            }
+        /// <summary>
+        /// Path of application file
+        /// </summary>
+        public string ApplicationPath => Path.Combine(Application.dataPath, "Morph", "application.json");
 
-            return new DirectoryInfo(folderPath);
-        }
+        #endregion
 
-        private DirectoryInfo CreateApplicationsFolderIfNeeded()
+        #region Private Methods
+
+        private static DirectoryInfo CreateApplicationsFolderIfNeeded()
         {
             string applicationsPath = Path.Combine(Application.dataPath, "..", "Applications");
-            return CreateFolderIfNeeded(applicationsPath);
+            return FileUtils.CreateFolderIfNeeded(applicationsPath);
         }
 
-        private DirectoryInfo CreateDependenciesFolderIfNeeded()
+        private static DirectoryInfo CreateDependenciesFolderIfNeeded()
         {
             string dependenciesPath = Path.Combine(Application.dataPath, "..", "Dependencies");
-            return CreateFolderIfNeeded(dependenciesPath);
+            return FileUtils.CreateFolderIfNeeded(dependenciesPath);
         }
 
-        void Awake()
+        /// <summary>
+        /// Is there specified dependency inside the project ?
+        /// </summary>
+        /// <param name="dependency">Dependency to test</param>
+        /// <returns>true if dependency found</returns>
+        private static bool HasDependency(string dependency)
+        {
+            var directories = Directory.EnumerateDirectories(Path.Combine(Application.dataPath), "*", SearchOption.AllDirectories);
+            return directories.Any(directory => directory.EndsWith(dependency));
+        }
+
+        /// <summary>
+        /// Check if some applications have the same name
+        /// </summary>
+        /// <returns>true if some applications have the same name</returns>
+        private bool CheckForDuplicateApplicationName()
+        {
+            var appNames = _applications.Select(app => app.Name).ToList();
+            return appNames.Count != appNames.Distinct().Count();
+        }
+
+        private void ChangeCurrentApplication(int applicationIndex)
+        {
+            File.WriteAllText(ApplicationPath, JsonUtility.ToJson(_applications[applicationIndex]));
+            _selectedApplication = applicationIndex;
+
+            Refresh();
+        }
+
+        /// <summary>
+        /// Is current application scene in the build settings ?
+        /// </summary>
+        /// <returns></returns>
+        private bool HasApplicationSceneInBuildSettings()
+        {
+            return EditorBuildSettings.scenes.ToList().Any(scene =>
+                scene.path.Replace("Assets/", "").Replace(".unity", "") == _applications[_selectedApplication].Scene);
+        }
+
+        private void Refresh()
         {
             _applications = new List<ApplicationData>();
+            _missingDependencies = new List<string>();
 
             //Applications
             DirectoryInfo applicationsDirectoryInfo = CreateApplicationsFolderIfNeeded();
@@ -111,9 +115,64 @@ namespace Morph.Editor
 
                 Debug.LogFormat("Morph found application {0}", data.Name);
             }
+
+            //Find current application file
+            if (!File.Exists(ApplicationPath))
+            {
+                Debug.LogWarning("Morph couldn't find any applications in the folder");
+
+                //If no application file found, create one with first available application
+                if (_applications.Count > 0)
+                {
+                    File.WriteAllText(ApplicationPath, JsonUtility.ToJson(_applications[0]));
+                }
+                else
+                {
+                    Debug.LogWarning(
+                        "Morph couldn't find current application, and no application to use has been found");
+                    return;
+                }
+            }
+            else
+            {
+                ApplicationData appData = JsonUtility.FromJson<ApplicationData>(File.ReadAllText(ApplicationPath));
+                for (var applicationIndex = 0; applicationIndex < _applications.Count; applicationIndex++)
+                {
+                    if (appData.Equals(_applications[applicationIndex]))
+                    {
+                        _selectedApplication = applicationIndex;
+                        Debug.Log($"Morph current application is {appData.Name}");
+                        break;
+                    }
+                }
+            }
+
+            //Look if all dependencies are found
+            foreach (var dependency in _applications[_selectedApplication].Dependencies)
+            {
+                if (!HasDependency(dependency))
+                {
+                    _missingDependencies.Add(dependency);
+                }
+            }
         }
 
-        void OnGUI()
+        #endregion
+
+        #region Unity Specific
+
+        [MenuItem("Morph/Application")]
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow<MorphApplicationWindow>("Morph Application");
+        }
+
+        private void Awake()
+        {
+            Refresh();
+        }
+
+        private void OnGUI()
         {
             //Application
             GUILayout.Label("Application Settings", EditorStyles.boldLabel);
@@ -124,9 +183,40 @@ namespace Morph.Editor
             //User has changed application
             if (selectedApplication != _selectedApplication)
             {
-                _selectedApplication = selectedApplication;
+                ChangeCurrentApplication(selectedApplication);
                 Debug.Log("Switch application");
             }
+
+            //Missing dependencies
+            foreach (var dependency in _missingDependencies)
+            {
+                EditorGUILayout.HelpBox($"Dependency {dependency} is missing", MessageType.Warning);
+            }
+
+            //Current application scene in build settings ?
+            if (!HasApplicationSceneInBuildSettings())
+            {
+                EditorGUILayout.HelpBox($"Application scene {_applications[_selectedApplication].Scene} is not in build settings", MessageType.Error);
+                if (GUILayout.Button("Open Build Settings"))
+                {
+                    EditorWindow.GetWindow<BuildPlayerWindow>();
+                }
+            }
+
+            //Duplicate names ?
+            if (CheckForDuplicateApplicationName())
+            {
+                EditorGUILayout.HelpBox("Several applications have the same name. It is not allowed.", MessageType.Error);
+            }
+
+            //Refresh button
+            if (GUILayout.Button("Refresh"))
+            {
+                Debug.Log("Morph application window is refreshing...");
+                Refresh();
+            }
         }
+
+        #endregion
     }
 }
