187cc4fc856d3e2b33e5d23ac9e2c4118b8b3c7a
Renaud Lefrançoise <renaud.lefrancoise@gmail.com>
2019-03-04 22:07:02 +0100

    Update test cube example

--- Assets/Morph-Examples/MorphExampleTestCube.cs
+++ Assets/Morph-Examples/MorphExampleTestCube.cs
@@ -4,16 +4,25 @@ namespace Morph.Examples
 {
     public class MorphExampleTestCube : MonoBehaviour
     {
+        [SerializeField]
+        private Color initialColor = Color.cyan;
+        [SerializeField]
+        private Color focusColor = Color.green;
+        [SerializeField]
+        private Color grabColor = Color.yellow;
+        
         private bool _rotate;
+        
+        private static readonly int EmissionColor = Shader.PropertyToID("_EmissionColor");
 
         public void OnFocusEnter()
         {
-            GetComponent<Renderer>().material.color = Color.green;
+            GetComponent<Renderer>().material.SetColor(EmissionColor, focusColor);
         }
 
         public void OnFocusExit()
         {
-            GetComponent<Renderer>().material.color = Color.white;
+            GetComponent<Renderer>().material.SetColor(EmissionColor, initialColor);
         }
 
         public void OnSelect()
@@ -23,15 +32,20 @@ namespace Morph.Examples
 
         public void OnGrabbed()
         {
-            GetComponent<Renderer>().material.color = Color.cyan;
+            GetComponent<Renderer>().material.SetColor(EmissionColor, grabColor);
         }
 
         public void OnRelease()
         {
-            GetComponent<Renderer>().material.color = Color.white;
+            GetComponent<Renderer>().material.SetColor(EmissionColor, initialColor);
         }
 
-        void Update()
+        private void Start()
+        {
+            OnFocusExit();
+        }
+
+        private void Update()
         {
             if (!_rotate) return;
 
